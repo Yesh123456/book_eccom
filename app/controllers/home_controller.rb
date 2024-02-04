8eb1fd5dcd485d2c045f5303f59d3fa3164203cb
Yesh123456 <yeshjadav4@gmail.com>
2024-02-04 20:40:42 +0530

    Pushed few Changes on Users level

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
 
 	def index
-		
+		@main_categories = Category.all
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -23,4 +23,6 @@ Rails.application.routes.draw do
 
   get "admin" => "admin#index"
 
+  get "category" => "category#index"
+
 end
